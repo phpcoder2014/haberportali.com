84608934771e66f4a906db9a029a13186c185b50
Nihat Şendil <Coder2015>
2015-02-13 14:29:25 +0200

    13022015
    güncelleme

---
+++ backup/basinda_biz.php
@@ -0,0 +1,170 @@
+<?php require_once("includes/db.php"); ?>
+<!DOCTYPE html>
+<!--[if IE 8]>    <html class="no-js ie8 ie" lang="en"> <![endif]-->
+<!--[if IE 9]>    <html class="no-js ie9 ie" lang="en"> <![endif]-->
+<!--[if gt IE 9]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
+
+	<head>
+		<meta charset="utf-8">
+		<title>Ak Parti | Karacabey İlçe Başkanlığı</title>
+		<meta name="description" content="">
+		<meta name="author" content="Ak Parti | www.karacabeyakparti.org.tr">
+		<meta name="robots" content="index, follow">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link href='http://fonts.googleapis.com/css?family=Rajdhani|Abel|Open+Sans+Condensed:700|Lora&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
+		<meta name="google-site-verification" content="uKA6-3gAxHu4nP0PTtcuL80DQ8sCbXl7WnFyt2JNfjY" />
+		<!-- Styles -->
+		<link rel="stylesheet" href="styleswitcher/css/styleswitcher.css">
+		<link rel="stylesheet" type="text/css" href="css/frenzy-orange.css" title="frenzy-orange" />
+
+        <script type="text/javascript" src="js/styleswitcher.js"></script>
+		
+		<!-- Fav and touch icons -->
+		<link rel="shortcut icon" href="img/ico/photo.png">
+
+		<link rel="apple-touch-icon-precomposed" sizes="114x114" href="img/ico/apple-touch-icon-114-precomposed.png">
+		<link rel="apple-touch-icon-precomposed" sizes="72x72" href="img/ico/apple-touch-icon-72-precomposed.png">
+		<link rel="apple-touch-icon-precomposed" href="img/ico/apple-touch-icon-57-precomposed.png">
+		<link rel="stylesheet" type="text/css" href="css/plugins/bootstrap-image-gallery.css">
+		<!-- JS Libs -->
+		
+		<script>window.jQuery || document.write('<script src="js/libs/jquery.js"><\/script>')</script>
+
+		<script src="js/libs/modernizr.js"></script>
+		<script src="js/libs/selectivizr-min.js"></script>
+		
+		<script src="styleswitcher/js/styleswitcher.js"></script>
+		<script src="js/bootstrap/bootstrap.js"></script>
+		<script src="js/plugins/slides.jquery.js"></script>
+		<script type="text/javascript" src="js/plugins/load-image.min.js"></script>
+		<script type="text/javascript" src="js/bootstrap/bootstrap-image-gallery.min.js"></script>
+		<script src="js/custom.js"></script>
+
+	</head>
+	<body style="background-image: url(img/assets/bg/pattern/bg-grid.png);"><br/><br/><br/>
+		<script>
+		  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+		  ga('create', 'UA-57811713-1', 'auto');
+		  ga('send', 'pageview');
+	</script>
+		<div id="bonfire-switcher-wrapper">
+		</div>
+		<div id="bg" class="hidden-phone">
+		<img src="img/assets/bg/kongre_bg.jpg" alt="Ak Parti" />
+		</div>
+
+
+		<!--///////////////////////////// Header Area //////////////////////////////////-->
+
+		<!-- Header Start -->
+			<?php require("includes/menu.php"); ?>
+
+
+
+		<!--///////////////////////////// Main Content Area //////////////////////////////////-->
+
+		<!-- Main Content Wrapper Start -->
+		<div class="container" role="main">
+			<!-- Main Content -->
+			<section id="main-content" class="row">
+							
+				<div id="content" class="span9">
+				  
+					<div class="post-single" id="post">
+						<div class="content-outer">
+							<div class="content-inner">
+								<div id="gallery">
+								<h2 style="font-family: 'Rajdhani', sans-serif;">İLETİŞİM BİLGİLERİ</h2>
+									<div id="gallery-list" class="row-fluid">
+										<div class="separator"></div>
+									<iframe width="810" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=tr&amp;geocode=&amp;q=Ak+Parti-karacabey+%C4%B0l%C3%A7e+Ba%C5%9Fkanl%C4%B1%C4%9F%C4%B1,+Karacabey,+T%C3%BCrkiye&amp;aq=2&amp;oq=akparti+kara&amp;sll=37.0625,-95.677068&amp;sspn=39.916234,86.572266&amp;ie=UTF8&amp;hq=Ak+Parti-karacabey+%C4%B0l%C3%A7e+Ba%C5%9Fkanl%C4%B1%C4%9F%C4%B1,&amp;hnear=Karacabey,+T%C3%BCrkiye&amp;ll=40.216557,28.359224&amp;spn=0.014947,0.006295&amp;t=m&amp;output=embed"></iframe>																		
+										 										
+									</div>
+									<div class="separator"></div>
+								</div>
+								<div id="gallery">
+								<h4 style="font-family: 'Rajdhani', sans-serif;">DÜŞÜNCE VE ÖNERİLERİNİZ</h4>
+								<div class="separator"></div><br/>
+								<div id="mesaj"></div>
+									<form>
+									<div class="row">
+										<div class="span4">
+											<div class="form-padding">
+												<label for="f1">Adınız (zorunlu):</label>
+												<input type="text" name="adi" placeholder="lütfen adınızı giriniz.." class="span4" value="">
+												<label for="f2">Telefon:</label>
+												<input type="text" name="telefon" placeholder="irtibat numaranız.." id="f2" class="span4" value="" size="40">
+											</div>
+										</div>
+										<div class="span4">
+												<div class="form-padding">
+													<label for="f1">Soyadınız (zorunlu):</label>
+													<input type="text" name="soyadi" placeholder="lütfen soyadınızı giriniz.." id="f1" class="span4" value="">
+													<label for="f2">E-Posta:</label>
+													<input type="email" name="eposta" placeholder="e-posta adresiniz.." id="f2" class="span4" value="" size="40">
+												</div>
+										</div>
+										<div class="span8">
+												<div class="form-padding">
+													<label for="f5">Mesajınız (zorunlu):</label>
+													<textarea name="mesaj" placeholder="bize iletmek istediğiniz mesajı detaylı bir şekilde yazınız.." class="span8" id="f5" cols="40" rows="10"></textarea>	
+													<input type="submit" onclick="javascript:Mesaj();" class="btn btn-inverse" value="Bilgileri Gönder">
+												</div>
+										</div>
+									</div>
+									</form>
+									
+								<script type="text/javascript"> 
+									function Mesaj() {
+										$.ajax({ 
+											type: "POST",
+											url: "contact_post.php",
+											data: $('form').serialize(), 
+											error:function(){ $('#mesaj').html("Bir hata algılandı."); },
+											success: function(veri) { 
+											$('.span4').val('');
+											$('.span8').val('');
+											$('#mesaj').html(veri);
+											}
+											
+										}); }
+								</script>
+									
+								</div>
+							</div>
+
+							<!-- content inner -->
+						</div>
+						<!-- content outer -->
+					</div>				
+				</div>
+				    <!-- Sidebar Right -->
+				<?php require_once("includes/right-sidebar.php"); ?>
+
+			</section>
+			<!-- End Main Content -->
+		</div>
+		<!-- Main Content Wrapper End -->
+
+		<!--///////////////////////////// Footer Area //////////////////////////////////-->
+		<?php require("includes/footer.php"); ?>
+		<!-- modal-gallery is the modal dialog used for the image gallery -->
+		<div id="modal-gallery" class="modal modal-gallery hide fade" tabindex="-1">
+		    <div class="modal-header">
+		        <a class="close" data-dismiss="modal">&times;</a>
+		        <h3 class="modal-title"></h3>
+		    </div>
+		    <div class="modal-body"><div class="modal-image"></div></div>
+		    <div class="modal-footer">
+		        <a class="btn btn-primary modal-next">ILERI <i class="icon-arrow-right icon-white"></i></a>
+		        <a class="btn btn-info modal-prev"><i class="icon-arrow-left icon-white"></i> GERI</a>
+		        <a class="btn btn-success modal-play modal-slideshow" data-slideshow="5000"><i class="icon-play icon-white"></i> GOSTERI</a>
+		        <a class="btn modal-download" target="_blank"><i class="icon-download"></i> INDIR</a>
+		    </div>
+		</div>
+	</body>
+</html>
